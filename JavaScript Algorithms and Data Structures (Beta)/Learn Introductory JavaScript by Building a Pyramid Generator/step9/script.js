14037d32a40f70e50858748c8417252fbaade2e2
Anton Lushchay <av.lushchay17@gmail.com>
2024-05-31 00:30:50 +0200

    step9

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step9/script.js
@@ -0,0 +1,8 @@
+// Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
