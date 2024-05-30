b3c2e2691538922da4637c374233bbef3618e248
Anton Lushchay <av.lushchay17@gmail.com>
2024-05-31 00:37:51 +0200

    step11

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step11/script.js
@@ -0,0 +1,6 @@
+// Remove both console.log statements, and the character reassignment.
+
+// Also remove your secondCharacter variable, but leave the character initialization unchanged.
+
+
+let character = 'Hello';
