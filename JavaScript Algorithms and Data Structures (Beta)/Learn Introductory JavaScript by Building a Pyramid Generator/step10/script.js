18346e6ef9ee7fbe1587432227a147003e7449d1
Anton Lushchay <av.lushchay17@gmail.com>
2024-05-31 00:34:35 +0200

    step10

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step10/script.js
@@ -0,0 +1,10 @@
+// change your secondCharacter assignment from "Test" to your character variable.
+
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+secondCharacter = character;
+console.log(secondCharacter);
