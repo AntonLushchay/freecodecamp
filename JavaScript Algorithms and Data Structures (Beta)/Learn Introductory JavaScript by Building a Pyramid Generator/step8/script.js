102519b9c55bf10c653157f3dd0c549e588356a0
Anton Lushchay <av.lushchay17@gmail.com>
2024-05-31 00:27:46 +0200

    step8

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step8/script.js
@@ -0,0 +1,7 @@
+// Add a console.log() to see what the value of your secondCharacter variable is.
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
