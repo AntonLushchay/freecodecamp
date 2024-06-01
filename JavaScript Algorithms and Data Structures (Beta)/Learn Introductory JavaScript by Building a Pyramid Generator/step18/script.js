2e4c2fc552f580d0757d8e64a4ecc2bb801e088d
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-01 12:38:44 +0200

    step18

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step18/script.js
@@ -0,0 +1,5 @@
+// Change your rows declaration to be an array with the strings "Naomi", "Quincy", and "CamperChan".
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
