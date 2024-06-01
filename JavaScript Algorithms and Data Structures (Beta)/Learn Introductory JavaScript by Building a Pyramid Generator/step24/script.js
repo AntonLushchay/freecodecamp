778fb6393ecd8566d9a074334b2a0e0c28391c1a
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-01 13:00:39 +0200

    step24

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step24/script.js
@@ -0,0 +1,8 @@
+// Create a new variable called popped and assign it the result of rows.pop(). Then, log popped to the console.
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop(); 
+console.log(popped );
