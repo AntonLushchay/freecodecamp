0510b21eea7b0e3f8be11a120af13fdc45090462
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-01 12:58:12 +0200

    step23

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step23/script.js
@@ -0,0 +1,7 @@
+// Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
