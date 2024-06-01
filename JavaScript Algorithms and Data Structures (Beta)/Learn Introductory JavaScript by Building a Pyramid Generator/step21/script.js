ec7b836bc1012e2e1583916dfed350760010863b
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-01 12:51:29 +0200

    step21

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step21/script.js
@@ -0,0 +1,7 @@
+// Update your rows[2] to dynamically access the last element in the rows array. 
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+console.log(rows[rows.length - 1]);
