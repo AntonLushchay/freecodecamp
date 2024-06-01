727cc406916a3f2e15f64541bf261477cbcaa8ad
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-01 12:41:54 +0200

    step19

---
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/step19/script.js
@@ -0,0 +1,6 @@
+// Use console.log and bracket notation to print the first value in your rows array.
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
