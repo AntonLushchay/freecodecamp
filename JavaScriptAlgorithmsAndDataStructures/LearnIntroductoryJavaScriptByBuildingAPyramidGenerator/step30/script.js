8a2ef58ce59426545f1f46684e26da9d527cce00
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-03 15:57:29 +0200

    step30

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step30/script.js
@@ -0,0 +1,7 @@
+// construct a for loop that includes the terms "iterator", "condition", and "iteration" for the three components. Keep the loop body, the section within the curly braces {}, empty.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for ("iterator"; "condition"; "iteration") {}
