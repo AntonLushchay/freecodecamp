8dc78507ccfa0cc466054d4e4102ca314ec2d567
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-03 16:36:33 +0200

    step35

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step35/script.js
@@ -0,0 +1,9 @@
+// Replace your log statement with a statement to push i to your rows array.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
