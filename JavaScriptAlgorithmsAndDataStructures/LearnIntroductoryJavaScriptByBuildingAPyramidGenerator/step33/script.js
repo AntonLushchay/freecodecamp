856a77ead8f4980a8e378af71e4bbab7ed72dd1b
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-03 16:32:40 +0200

    step33

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step33/script.js
@@ -0,0 +1,9 @@
+// Use that syntax to replace your "iteration" string with a reassignment statement that increases i by one.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+
+}
