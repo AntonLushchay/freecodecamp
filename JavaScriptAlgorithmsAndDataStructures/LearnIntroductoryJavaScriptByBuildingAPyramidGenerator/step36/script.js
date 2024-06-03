552a5f3f334537b7349c8180ee4be6312bd068bf
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-03 16:38:11 +0200

    step36

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step36/script.js
@@ -0,0 +1,11 @@
+// Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = "";
