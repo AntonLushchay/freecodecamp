925c05f3782b7d520ef9e29dff585ea093ef4989
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-03 16:26:51 +0200

    step32

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step32/script.js
@@ -0,0 +1,9 @@
+// Replace your "condition" string with a condition to check if i is less than count.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; "iteration") {
+
+}
