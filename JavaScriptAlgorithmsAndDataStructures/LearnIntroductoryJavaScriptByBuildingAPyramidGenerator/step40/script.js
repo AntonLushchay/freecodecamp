607c8015b1ca9293dc6f7bc9ef91bd1f626be3a6
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-04 16:46:25 +0200

    step40

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step40/script.js
@@ -0,0 +1,16 @@
+// Use a second addition operator to concatenate a new line between the existing result value and the added row value.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+console.log(result)
