adaa0efef99748011572158b67e53235bf1a41e0
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-04 16:51:46 +0200

    step42

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step42/script.js
@@ -0,0 +1,17 @@
+// Use the .repeat() method on your character, and give it i for the number.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
