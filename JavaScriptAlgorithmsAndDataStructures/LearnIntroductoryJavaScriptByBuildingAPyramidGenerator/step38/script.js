0325aa6f4c9b3aa29f62dd6ac1e2bba7010f8e98
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-04 16:33:34 +0200

    step38

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step38/script.js
@@ -0,0 +1,16 @@
+// Create a for...of loop to iterate through your rows array, assigning each value to a row variable
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
+
+for (const row of rows) {
+
+}
+console.log(result);
