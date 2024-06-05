0382a35d2be869a1376dfda748dd6e037abac21b
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-05 03:16:58 +0200

    concatjs ended

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/dist/concated.js
@@ -0,0 +1,487 @@
+// Step1
+// Declare a character variable in your code.
+
+let character;
+
+
+
+// Step2
+// Initialize your character variable by assigning it the value "Hello" during its declaration.
+
+let character = 'Hello';
+
+
+
+// Step3
+// Change your "Hello" string to use single quotes.
+
+let character = 'Hello';
+
+
+
+// Step4
+// Print the value of the character variable to the console. Then, click the "Console" button to view the JavaScript console.
+
+let character = 'Hello';
+console.log(character);
+
+
+
+// Step5
+// After your console.log, assign the value "World" to your character variable.
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+
+
+
+// Step6
+// log your character variable to the console again. You should see the string "Hello", then the string "World", in the console.
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+console.log(character);
+
+
+
+// Step7
+// Use camel case to declare a new secondCharacter variable.
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+console.log(character);
+let secondCharacter;
+
+
+
+// Step8
+// Add a console.log() to see what the value of your secondCharacter variable is.
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+let secondCharacter;
+console.log(secondCharacter);
+
+
+
+// Step9
+// Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+let secondCharacter;
+secondCharacter = 'Test';
+console.log(secondCharacter);
+
+
+
+// Step10
+// change your secondCharacter assignment from "Test" to your character variable.
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+let secondCharacter;
+secondCharacter = 'Test';
+secondCharacter = character;
+console.log(secondCharacter);
+
+
+
+// Step11
+// Remove both console.log statements, and the character reassignment.
+
+// Also remove your secondCharacter variable, but leave the character initialization unchanged.
+
+let character = 'Hello';
+
+
+
+// Step12
+// Use the let keyword to declare a profession variable and an age variable. Initialize profession with the string "teacher", but do not initialize age with any value.
+
+// Log both of your variables to the console to see the results.
+
+let character = 'Hello';
+let profession = 'teacher';
+let age;
+console.log(profession);
+console.log(age);
+
+
+
+// Step13
+// remove the code you wrote for your review.
+
+// Do not remove your character variable.
+
+let character = 'Hello';
+
+
+
+// Step14
+// Use let to declare a count variable. Assign it the number 8
+
+let character = 'Hello';
+let count = 8;
+
+
+
+// Step15
+// With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
+
+let character = 'Hello';
+let count = 8;
+console.log(count + 1);
+
+
+
+// Step16
+// You can also perform subtraction (-), multiplication (*), and division (/). Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.
+
+let character = 'Hello';
+let count = 8;
+
+
+
+// Step17
+// Declare a rows variable and assign it an empty array.
+
+let character = 'Hello';
+let count = 8;
+let rows = [];
+
+
+
+// Step18
+// Change your rows declaration to be an array with the strings "Naomi", "Quincy", and "CamperChan".
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+
+
+
+// Step19
+// Use console.log and bracket notation to print the first value in your rows array.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+console.log(rows[0]);
+
+
+
+// Step20
+// Update the third element of your rows array to be the number 10. Then print the rows array to your console.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);
+
+
+
+// Step21
+// Update your rows[2] to dynamically access the last element in the rows array.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+console.log(rows[0]);
+console.log(rows[rows.length - 1]);
+
+
+
+// Step22
+// For now, remove your first console log and your rows[rows.length - 1] assignment. Leave the second rows log statement for later.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+console.log(rows);
+
+
+
+// Step23
+// Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+rows.push('freeCodeCamp');
+console.log(rows);
+
+
+
+// Step24
+// Create a new variable called popped and assign it the result of rows.pop(). Then, log popped to the console.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+rows.push('freeCodeCamp');
+let popped = rows.pop();
+console.log(popped);
+
+
+
+// Step25
+// what does .push() return? Assign your existing rows.push() to a new pushed variable, and log it.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+let pushed = rows.push('freeCodeCamp');
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
+
+
+
+// Step26
+// remove all of your .push() and .pop() calls, and your console.log statements.
+
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+
+
+
+// Step27
+// Change your rows declaration to be assigned an empty array again.
+
+// Also, change your 'Hello' string to use double quotes again.
+
+let character = 'Hello';
+let count = 8;
+let rows = [];
+
+
+
+// Step28
+// Replace your let keywords with const.
+
+const character = 'Hello';
+const count = 8;
+const rows = [];
+
+
+
+// Step29
+// Change the value of character to be the hash character "#".
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+
+
+// Step30
+// construct a for loop that includes the terms "iterator", "condition", and "iteration" for the three components. Keep the loop body, the section within the curly braces {}, empty.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for ('iterator'; 'condition'; 'iteration') {}
+
+
+
+// Step31
+// Replace the string "iterator" with a let declaration for the variable i. Assign it the value 0 to start. This will give the i variable the value 0 the first time your loop runs.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; 'condition'; 'iteration') {}
+
+
+
+// Step32
+// Replace your "condition" string with a condition to check if i is less than count.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; 'iteration') {}
+
+
+
+// Step33
+// Use that syntax to replace your "iteration" string with a reassignment statement that increases i by one.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {}
+
+
+
+// Step34
+// Inside the body of the loop, print the value of the i iterator and see what happens.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i);
+}
+
+
+
+// Step35
+// Replace your log statement with a statement to push i to your rows array.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+
+
+// Step36
+// Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+let result = '';
+
+
+
+// Step37
+// Add a log statement to print the value of result. Depending on which console you use, you may not see anything printed.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+let result = '';
+
+console.log(result);
+
+
+
+// Step38
+// Create a for...of loop to iterate through your rows array, assigning each value to a row variable
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+let result = '';
+
+for (const row of rows) {
+}
+console.log(result);
+
+
+
+// Step39
+// In your for...of loop, use the addition operator to concatenate the row value to the result value.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + row;
+}
+console.log(result);
+
+
+
+// Step40
+// Use a second addition operator to concatenate a new line between the existing result value and the added row value.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+console.log(result);
+
+
+
+// Step41
+// Instead of pushing i to the array, push the value of your character variable.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character);
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
+
+
+
+// Step42
+// Use the .repeat() method on your character, and give it i for the number.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,25 +1,70 @@
 import { src, dest, series } from 'gulp';
 import concat from 'gulp-concat';
 import { deleteAsync } from 'del';
+import order from 'gulp-order';
+import fs from 'fs';
+import path from 'path';
 
 const paths = {
     lern1: {
         src: 'JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/**/*.js',
-        dest: 'JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/',
+        dest: 'JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/dist',
+    },
+    lern2: {
+        src: 'JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator',
     },
 };
 
+function countFolders(folderPath) {
+    let folderCount = 0;
+
+    const files = fs.readdirSync(folderPath);
+    for (const file of files) {
+        const filePath = path.join(folderPath, file);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            folderCount++;
+        }
+    }
+
+    return folderCount;
+}
+
 export function concatjs() {
-    return src(paths.lern1.src).pipe(concat('concated.js')).pipe(dest(paths.lern1.dest));
+    function countFolders(folderPath) {
+        let folderCount = 0;
+
+        const files = fs.readdirSync(folderPath);
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const stats = fs.statSync(filePath);
+            if (stats.isDirectory()) {
+                folderCount++;
+            }
+        }
+
+        return folderCount;
+    }
+
+    const folderPath = paths.lern2.src; // Путь к папке, в которой нужно посчитать папки
+    const folderCount = countFolders(folderPath);
+    console.log(`В папке ${folderPath} ${folderCount} папок.`);
+
+    const sortedFolders = [...Array(folderCount)].map((_, i) => `Step${i + 1}`);
+    return src(
+        sortedFolders.map(
+            (folder) =>
+                `JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/${folder}/script.js`
+        )
+    )
+        .pipe(order(['name', 'desc']))
+        .pipe(concat('concated.js', { newLine: '\n\n\n' }))
+        .pipe(dest(paths.lern1.dest));
 }
 
 export const clean = () => deleteAsync(paths.lern1.dest);
 
-// function watchFiles() {
-//     watch(paths.lern1.src, concatjs);
-// }
-// export { watchFiles as watch };
-
 const build = series(clean, concatjs);
 
 export default build;
+
