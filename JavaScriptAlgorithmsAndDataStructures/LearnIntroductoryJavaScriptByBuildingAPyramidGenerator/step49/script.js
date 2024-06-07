c6423418e43365d67c5346168bd8cc02bd5075c3
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-07 19:06:06 +0200

    step49

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step49/script.js
@@ -0,0 +1,29 @@
+// Step 49
+// To add a parameter to your function, you need to add a variable name inside the parentheses. For example, this demo function has a name parameter:
+
+// function demo(name) {
+
+// }
+// name sounds like a useful parameter, so go ahead and add it to your padRow function.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+    return 'Hello!';
+}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i + 1));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
