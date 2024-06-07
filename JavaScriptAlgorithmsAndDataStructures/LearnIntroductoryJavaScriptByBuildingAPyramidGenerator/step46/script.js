423ed62b09164f1c3e5bf203bbf1d3c9fc0c7003
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-07 18:46:48 +0200

    step46

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step46/script.js
@@ -0,0 +1,20 @@
+// Step 46
+// To see the result of calling your padRow function, declare a call variable and assign your existing padRow call to that variable.
+
+const character = '#';
+const count = 8;
+const rows = [];
+function padRow() {}
+const call = padRow();
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i + 1));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
