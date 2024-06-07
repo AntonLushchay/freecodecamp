f499812f5ce028e47e0fd3894b54f4be12967234
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-07 23:46:49 +0200

    step57

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step57/script.js
@@ -0,0 +1,22 @@
+// Step 57
+// Go ahead and remove the test declaration from your padRow function. Also, remove the return statement, so your function is empty again.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+function padRow() {}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i + 1));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
