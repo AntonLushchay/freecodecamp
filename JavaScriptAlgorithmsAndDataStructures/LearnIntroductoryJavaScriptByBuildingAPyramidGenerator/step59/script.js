36badc5ab5676cb3f4964e944e3d7504c71af2ee
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-07 23:50:25 +0200

    step59

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step59/script.js
@@ -0,0 +1,20 @@
+// Step 59
+// Give your padRow function a rowNumber and rowCount parameter. Multiple parameters are separated by a comma
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {}
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i + 1));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
