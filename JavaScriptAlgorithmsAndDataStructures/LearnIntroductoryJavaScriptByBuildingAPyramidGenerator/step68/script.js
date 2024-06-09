e95d6681e65fb26aa4f1f972a29e8b9f27a137ae
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-09 16:35:06 +0200

    step68

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step68/script.js
@@ -0,0 +1,26 @@
+// Step 68
+// Update your iterator to start at 1 instead of 0.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return (
+        ' '.repeat(rowCount - rowNumber) +
+        character.repeat(2 * rowNumber - 1) +
+        ' '.repeat(rowCount - rowNumber)
+    );
+}
+
+for (let i = 1; i < count; i++) {
+    rows.push(padRow(i + 1, count));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
