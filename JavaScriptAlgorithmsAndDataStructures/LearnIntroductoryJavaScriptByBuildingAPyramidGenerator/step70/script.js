b0b500257035c22a20a93671d40b1fa28f0e6221
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-09 16:43:23 +0200

    step70

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step70/script.js
@@ -0,0 +1,26 @@
+// Step 70
+// pdate your loop condition to run while i is less than or equal to count.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return (
+        ' '.repeat(rowCount - rowNumber) +
+        character.repeat(2 * rowNumber - 1) +
+        ' '.repeat(rowCount - rowNumber)
+    );
+}
+
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
