2b30df932fcb1824d67ecafedd6a8d70884a03b2
Anton Lushchay <av.lushchay17@gmail.com>
2024-06-09 16:39:53 +0200

    step69

---
+++ JavaScriptAlgorithmsAndDataStructures/LearnIntroductoryJavaScriptByBuildingAPyramidGenerator/step69/script.js
@@ -0,0 +1,26 @@
+// Step 69
+// Update the first argument of your padRow call to be i.
+
+const character = '#';
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return (
+        ' '.repeat(rowCount - rowNumber) +
+        character.repeat(2 * rowNumber - 1) +
+        ' '.repeat(rowCount - rowNumber)
+    );
+}
+
+for (let i = 1; i < count; i++) {
+    rows.push(padRow(i, count));
+}
+
+let result = '';
+
+for (const row of rows) {
+    result = result + '\n' + row;
+}
+
+console.log(result);
