a6921107203b0bc250043c479bde361a63a4d866
Anton Lushchay <av.lushchay17@gmail.com>
2024-07-29 18:56:32 +0200

    Step 53

--- Java Script A lgorithms And Data Structures/Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ Java Script A lgorithms And Data Structures/Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -18,12 +18,6 @@ const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
 const locations = [];
 
-const cat = {
-    name: 'Whiskers',
-    'Number of legs': 4,
-};
-console.log(cat['Number of legs']);
-
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
